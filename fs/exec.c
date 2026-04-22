072c477774822445afe9dfe15ae1a651b8ceda23
bone-machine <12587836+bone-machine@users.noreply.github.com>
2026-04-22 16:51:17 -0300

    KernelSU-Next: Switch to manual hooks implementation instead of KPROBES hook
    Thanks to backslashxx - https://github.com/backslashxx/KernelSU/issues/5#event-24583207399

--- fs/exec.c
+++ fs/exec.c
@@ -1940,6 +1940,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
+#ifdef CONFIG_KSU
+	__attribute__((hot))
+	extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+#endif
+
 int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
@@ -1954,6 +1959,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	#ifdef CONFIG_KSU
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
